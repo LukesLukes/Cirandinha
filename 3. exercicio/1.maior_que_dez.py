71637aa45662101b87bbeec948d8a309bbd5aece
LukesLukes <luccasoliveiradossantos93@gmail.com>
2025-02-19 17:46:13 +0000

    92360 TESTS

---
+++ 3. exercicio/1.maior_que_dez.py
@@ -0,0 +1,25 @@
+import os
+os.system("clear")
+
+# Elaborar um algoritmo para solicitar ao usuário um valor e escrever a mensagem: É MAIOR QUE 10!
+
+# Se o valor lido  for maior que 10, caso contrário escrever: NÃO É MAIOR QUE 10!
+
+age = 11
+
+
+if age > 10 :
+   print("Maior que 10!")
+else:
+   print("Menor que 10.")
+
+
+
+print("Obrigado por ter respondido.")
+
+
+
+
+
+
+ 
