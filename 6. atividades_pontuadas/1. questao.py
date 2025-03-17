b3d906a4b41e3195d5895157f60baef3ef1e2bfd
LukesLukes <luccasoliveiradossantos93@gmail.com>
2025-03-17 18:47:17 +0000

    Test

---
+++ 6. atividades_pontuadas/1. questao.py
@@ -0,0 +1,28 @@
+import os
+os.system("clear || clls")
+
+# Faça um algoritmo que leia os valores A, B, C 
+# e imprima na tela se a soma de A + B é menor que C, caso contrário, imprima que a A + B é maior que C.
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+c = float(input("Digite o terceiro número: "))
+resultado = float
+
+if a + b < c:
+    print("A + B é maior que ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
